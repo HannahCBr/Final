f533b7a087b543c8909750d53b630eaba65ea4a3
HannahCBr <48269751+HannahCBr@users.noreply.github.com>
2019-04-24 03:10:37 -0500

    Update PatientFrame.java

--- PatientFrame.java
+++ PatientFrame.java
@@ -1,8 +1,9 @@
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.PriorityQueue;
-
 import javax.swing.*;
 
 /**
@@ -15,7 +16,6 @@ import javax.swing.*;
  */
 public class PatientFrame extends JFrame{
 	int limit = 0;
-	int one = 0,two = 0,three = 0;
 	ArrayList<String> array;
 	
 	JLabel fName = new JLabel("First Name: ");
@@ -42,10 +42,14 @@ public class PatientFrame extends JFrame{
 	
 	JButton submit = new JButton("Submit");
 	
+	JTextArea patientList = new JTextArea(20, 30);
+	JScrollPane scroll = new JScrollPane(patientList);
 	JPanel panel = new JPanel();
 	
+	Patient patient = new Patient();
 	SymptomTree external = new SymptomTree();
-	PriorityQueue<Object> queue = new PriorityQueue<>();
+	SymptomTree internal = new SymptomTree();
+	PriorityQueue<Patient> queue = new PriorityQueue<>();
 	public PatientFrame() {
 		external.insert("1st Degree Burn", 5, 1);external.insert("2nd Degree Burn", 50, 2);external.insert("3rd Degree Burn", 95, 3);external.insert("Animal Bite", 40, 4);external.insert("Broken Bone Protrusion", 75, 5);external.insert("Cyst", 5, 6);external.insert("Joint Dislocation", 15, 7);external.insert("Lost Limb", 100, 8);external.insert("Necrotic Tissue", 90, 9);external.insert("Stab Wound:Critical", 100, 10);external.insert("Stab Wound:Non-critical", 65, 11);
 		
@@ -75,45 +79,49 @@ public class PatientFrame extends JFrame{
 		submitListener sub = new submitListener();
 		submit.addActionListener(sub);
 		
+		panel.add(patientList);
 		add(panel);
 	}
 	
 	class submitListener implements ActionListener{ //FINISH
 		@Override
 		public void actionPerformed(ActionEvent e) {	
-			Patient patient = new Patient();
-			patient.setfName(fNameField.getText());
-			patient.setlName(lNameField.getText());
+			int one = 0,two = 0,three = 0;
 			if (exOrIn.getSelectedItem() == "Internal") {
 				if (internalChoice.getSelectedItem() != null) {
-					patient.setS1((String) internalChoice.getSelectedItem());
+					
 				}
 				if (internalChoice2.isVisible()==true) {
-					patient.setS2((String)internalChoice2.getSelectedItem());
+					
 				}
 				if (internalChoice3.isVisible()==true) {
-					patient.setS3((String)internalChoice3.getSelectedItem());
+					
 				}
+				queue.add(new Patient(internal.math(one, two, three), fNameField.getText(), lNameField.getText()));
 			} else {
 				if (externalChoice.getSelectedItem() != null) {
-					patient.setS1((String) externalChoice.getSelectedItem());
 					one = external.find(external.root,(String) externalChoice.getSelectedItem());
 				}
 				if (externalChoice2.isVisible()==true) {
-					patient.setS2((String) externalChoice2.getSelectedItem());
 					two = external.find(external.root,(String)externalChoice2.getSelectedItem());
 				}
 				if (externalChoice3.isVisible()==true) {
-					patient.setS3((String) externalChoice3.getSelectedItem());
 					three = external.find(external.root,(String)externalChoice3.getSelectedItem());
 				}
-				patient.setTotal(external.math(one, two, three));
-				System.out.println(patient.total);
+				queue.add(new Patient(external.math(one, two, three), fNameField.getText(), lNameField.getText()));
 			}
-			System.out.println(patient.toString());
-			//queue.add(patient);
-			
-			fNameField.setText("");lNameField.setText("");
+			fNameField.setText("");lNameField.setText(""); //clear patient info for next use
+			patientList.setText(null);
+			StringBuilder sb = new StringBuilder();
+			Object[] arr = queue.toArray(); //turns queue into array for easy access
+			String[] sa = Arrays.stream(arr).map(Object::toString).toArray(String[]::new); //convert again zzz
+			for (int i=0;i<arr.length;i++) {
+				String temp = sa[i];
+				temp = temp.replace("fName=", " "); temp = temp.replace(",", ""); temp = temp.replace("lName=", "");
+				sb.append(temp);
+				sb.append("\n");
+			}
+			patientList.insert(sb.toString(), 0); //prints patients onto list (+ADD TIME??)
 		}
 		
 	}
@@ -123,7 +131,6 @@ public class PatientFrame extends JFrame{
 		public void actionPerformed(ActionEvent e) {
 			String list = (String) exOrIn.getSelectedItem();
 			if (list == "Internal") {
-				SymptomTree internal = new SymptomTree();
 				internalChoice.setVisible(true);
 				externalChoice.setVisible(false);
 				add.setVisible(true);
