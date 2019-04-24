759a32eba610ceebceaed9c5b95930289e215e08
HannahCBr <48269751+HannahCBr@users.noreply.github.com>
2019-04-24 03:08:28 -0500

    Update Driver.java

--- Driver.java
+++ Driver.java
@@ -14,14 +14,7 @@ public class Driver {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-
-		SymptomTree internal = new SymptomTree();
-		
-		//EXTERNAL SYMPTOM LIST
-		//INTERNAL SYMPTOM LIST
-		
 		JFrame frame = new PatientFrame();
-		//PatientFrame panel = new PatientFrame();
 		
 		frame.setSize(500,500);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
